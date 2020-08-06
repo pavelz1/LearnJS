9fcf1d6269c6fe3fbdaec19ce910360d377e0ce0
PavelZ <pavelz@mail.ru>
2020-08-07 02:47:18 +0300

    rebase

---
+++ script.js
@@ -0,0 +1,16 @@
+// Объявим переменные
+let money = 10; // Деньги
+
+let income = 100; // доход
+
+let addExpenses = 50; // добавить расходы
+
+let deposit = 100; // депозит
+
+let mission = 'Цель'; // цель
+
+let period = '6 месяцев'; // период
+
+console.log(period);
+
+alert('Привет, чтобы увидеть console.log открой панель разработчика');
